dbc62b661863d4653ec93287f5f969b253ce355d
shamil9 <shamil9@gmail.com>
2016-11-09 13:40:53 +0100

    Refoctoring

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const app = require('express')()
-const request = require('request')
 const fs = require('fs')
 const sqlite3 = require("sqlite3").verbose()
+const User = require('./list')
 
 //Configuration
 const file = 'sqlite.db'
@@ -9,84 +9,45 @@ const exists = fs.existsSync(file)
 const db = new sqlite3.Database(file)
 const token = ''
 const tokenSet = ''
+
 db.serialize(function() {
     if (!exists)
         db.run("CREATE TABLE users (name TEXT, repo TEXT)")
 })
 
+//Affichage des commits
 app.get('/', (req, res) => {
     // Vérification du token
     if (req.param('token') !== token)
         return res.send('Token not valid')
 
-    let user = req.param('text') //le nom d'utilsateur sur slack
+    let param = req.param('text') //le nom d'utilsateur sur slack
     db.all('SELECT * FROM users', (err, row) => {
         if (err)
             return res.send(err)
 
+        let user = new User(param, row);
         //Affichage de la liste des utilisateurs
-        if (user === 'list') {
-            let response = []
-
-            row.forEach((item) => {
-                //Construction de réponse sur slack
-                let userObject = {
-                    author_name: item.name,
-                    title: item.repo,
-                    title_link: `https://github.com/${item.repo}`
-                }
-
-                response.push((userObject))
-            })
-
+        if (param === 'list') {
             return res.json({
                 text: "Liste des utilisateurs",
-                attachments: response
+                attachments: user.list()
             })
         }
 
         //Affichage des commits
         //Recherche d'utilisateur dans la DB
-        let dbUser = row.find((row) => row.name === user)
+        let dbUser = row.find((row) => row.name === param)
 
         //Réponse si utilisateur est introuvable
         if (typeof dbUser == 'undefined') {
             return res.send(`Aucun repo associé avec ${user}! Slap HIM!`)
         }
 
-        let response = []
-
-        //Options pour le request
-        let options = {
-            url: `https://api.github.com/repos/${dbUser.repo}/commits`,
-            json: true,
-            headers: {
-                'User-Agent': 'request'
-            }
-        }
-
-        //Appel à l'API de Github
-        request(options, (error, result, data) => {
-            //Contrôle si le repo existe
-            if (error)
-                return res.send(`Repo Introuvable ${error}`)
-
-            //Mise en forme de la réponse sur slack
-            data.forEach((key) => {
-                let message = {}
-                let commitDate = Date.parse(key.commit.author.date)
-                message.fallback = "Foo bar"
-                message.author_name = key.commit.author.name
-                message.title = key.commit.message
-                message.title_link = key.html_url
-                message.ts = commitDate / 1000
-
-                response.push(message)
-            })
-
+        user.fetchRepo(dbUser, (data) => {
             return res.json({
                 text: "Liste des commits",
-                attachments: response
+                attachments: user.formatMessage(data)
             })
         })
     })
---
+++ list.js
@@ -0,0 +1,63 @@
+class User {
+    constructor(param, dbResults) {
+        this.param = param
+        this.dbResults = dbResults
+        this.request = require('request')
+        this.commits = []
+    }
+
+    list() {
+        //Affichage de la liste des utilisateurs
+        if (this.param === 'list') {
+            let response = []
+
+            this.dbResults.forEach((item) => {
+                //Construction de réponse sur slack
+                let userObject = {
+                    author_name: item.name,
+                    title: item.repo,
+                    title_link: `https://github.com/${item.repo}`
+                }
+
+                response.push((userObject))
+            })
+
+            return response
+        }
+    }
+
+    fetchRepo(user, callback) {
+        //Options pour le request
+        let options = {
+            url: `https://api.github.com/repos/${user.repo}/commits`,
+            json: true,
+            headers: {
+                'User-Agent': 'request'
+            }
+        }
+
+        //Appel à l'API de Github
+        this.request(options, (error, result, data) => {
+            //Contrôle si le repo existe
+            if (error || data.message === 'Not found')
+                return (`Repo Introuvable ${error}`)
+
+            return callback(data)
+        })
+    }
+
+    formatMessage(data) {
+        //Mise en forme de la réponse sur slack
+        data.forEach((key) => {
+            this.commits.push({
+                fallback: "Foo bar",
+                author_name: key.commit.author.name,
+                title: key.commit.message,
+                title_link: key.html_url,
+                ts: Date.parse(key.commit.author.date) / 1000
+            })
+        })
+        return this.commits
+    }
+}
+module.exports = User
